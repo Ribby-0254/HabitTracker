c7961aec8112a7c0c45a7b7713f4da40bff4cd95
Timothy Tudosa <timtudosa6@gmail.com>
2021-03-04 21:15:37 -0600

    Created mongoose models for the backend

--- backend/models/customHabit.js
+++ backend/models/customHabit.js
@@ -3,7 +3,8 @@ const { Mongoose } = require("mongoose");
 
 const customHabitsSchema = new mongoose.Schema({
     name:{
-        type:String
+        type:String,
+        isRequired:true
     },
     description:{
         type:String,
