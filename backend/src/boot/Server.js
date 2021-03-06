590c7accaf676228fac3d73f1c8f49d76a696e67
Ribby-0254 <14286113+Ribby-0254@users.noreply.github.com>
2021-03-06 13:18:52 -0500

    Save work

--- backend/src/boot/Server.js
+++ backend/src/boot/Server.js
@@ -9,6 +9,10 @@ export default class Server{
         this.app = express();
     }
 
+    homePage(){
+        this.app.get('/', (req, res) => res.send("hello world"));
+    }
+
     /**
      * Starts HTTP server
      * Promisifies app.listen()
@@ -17,6 +21,7 @@ export default class Server{
     start(){
         return new Promise(
             (resolve, reject) => {
+                this.homePage();
                 const http = this.app.listen(process.env.PORT, "localhost", () => {
                     const { address, port } = http.address();
                     console.log(`Server running on localhost:${port}`);
