eb19e93f2392d9c14cd0fa953afc51fb54f9b681
Ribby-0254 <14286113+Ribby-0254@users.noreply.github.com>
2021-03-10 23:08:40 -0500

    Create Entity.js to provide common utility methods to model objects

---
+++ backend/src/models/Entity.js
@@ -0,0 +1,51 @@
+export default class {
+    /**
+     * Used to provide utility operations to models
+     */
+    constructor(){
+        /**
+         * @type {mongoose.Schema.Types.ObjectId}
+         */
+        this.id = '';
+    }
+
+    /**
+     * 
+     * @param {*} otherEntity
+     * @returns {boolean} true if equal, false if not equal
+     */
+    equals(otherEntity){
+        // check if otherEntity is typeof Entity
+        if(otherEntity instanceof Entity === false) return false;
+
+        // use id to determine if same object
+        return otherEntity.id  ? (otherEntity.id === this.id)? (this === otherEntity);
+
+    }
+
+    /**
+     * 
+     * @param {*} id
+     * @returns {boolean} true if equal, false if not equal
+     */
+    idEquals(id){
+        // check if this object even has an id yet
+        // if not, then just call the equals method
+        if(!this.id) return this.equals(id);
+
+        // Get string version of otherEntity
+        // ongoose ids are of type ObjectId
+        // we assume mongoose object id has toString() to convert it to string
+        const sanitizedId = typeof id !== 'string' ? id.toString() : id;
+
+        return this.id === sanitizedId;
+    }
+
+    /**
+     * Meant to be overridden to provide custom String desc. of object
+     * @returns {string}
+     */
+    toString(){
+        return this.id;
+    }
+}
